60beb5856139d62ec30e6143f72d7756a357094e
Stephen Trewick <stephent818@gmail.com>
2019-06-15 17:18:01 -0700

    messing around with different styles

--- src/app/add-project/add-project.component.ts
+++ src/app/add-project/add-project.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Project } from '../models/project.model';
 
 @Component({
   selector: 'app-add-project',
   templateUrl: './add-project.component.html',
   styleUrls: ['./add-project.component.css']
 })
-export class AddProjectComponent implements OnInit {
+export class AddProjectComponent {
+  @Output() sendProject = new EventEmitter();
+
+  
 
   constructor() { }
 
